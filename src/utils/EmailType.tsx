6ad6feb96dc037500ba7ce0533f63220d8321e8b
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2024-02-13 10:23:55 +0100

    Update EmailType.tsx
    Changed regex to match comma seperated mail addresses
    Modified design to better integrate in overall webcomponent design

--- src/utils/EmailType.tsx
+++ src/utils/EmailType.tsx
@@ -1,5 +1,5 @@
-import { GenericIdentifierType } from './GenericIdentifierType';
-import { FunctionalComponent, h } from '@stencil/core';
+import {GenericIdentifierType} from './GenericIdentifierType';
+import {FunctionalComponent, h} from '@stencil/core';
 
 /**
  * This class specifies a custom renderer for Email addresses.
@@ -11,7 +11,8 @@ export class EmailType extends GenericIdentifierType {
   }
 
   hasCorrectFormat(): boolean {
-    const regex = /^((\(.*\))?(\w|-|\+|_|\.)+(\(.*\))?@((((\(.*\))?\w+(\(.*\))?\.)+(\(.*\))?\w+(\(.*\))?\w+)|(\[((IPv6:((\w+:+)+\w+))|(\d+\.)+\d+)\]))\s*(,+|$)\s*)+$/
+    // const regex = /^((\(.*\))?(\w|-|\+|_|\.)+(\(.*\))?@((((\(.*\))?\w+(\(.*\))?\.)+(\(.*\))?\w+(\(.*\))?\w+)|(\[((IPv6:((\w+:+)+\w+))|(\d+\.)+\d+)\]))\s*(,+|$)\s*)+$/
+    const regex = /^(([\w\-\.]+@([\w-]+\.)+[\w-]{2,})(\s*,\s*)?)*$/gm;
     return regex.test(this.value);
   }
 
@@ -26,12 +27,15 @@ export class EmailType extends GenericIdentifierType {
   renderPreview(): FunctionalComponent<any> {
     // mail icon from: https://heroicons.com/ (MIT license)
     return (
-      <span class={'flex inline'}>
+      <span class={"items-center"}>
         {this.value.split(new RegExp(/\s*,\s*/)).filter(email => email.length > 0).map(email => {
           return (
-            <a href={'mailto:' + email} target='_blank' class={'flex inline font-mono text-sm text-blue-400 border border-slate-500 rounded-md px-1 py-2 mr-2'}>
-              <svg xmlns='http://www.w3.org/2000/svg' fill='none' aria-hidden='true' viewBox='0 0 24 24' stroke-width='1' stroke='black' height='20px' class={'mr-1'}>
-                <path stroke-linecap='round' stroke-linejoin='round' d='M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75' />
+            <a href={'mailto:' + email} target='_blank'
+               class={'items-center inline-flex font-mono text-sm text-blue-400 border border-slate-400 rounded-md px-1 py-0.5 mr-2'}>
+              <svg xmlns='http://www.w3.org/2000/svg' fill='none' aria-hidden='true' viewBox='0 0 24 24'
+                   stroke-width='1' stroke='black' height='20px' class={'mr-1'}>
+                <path stroke-linecap='round' stroke-linejoin='round'
+                      d='M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75'/>
               </svg>
               <span class={'ml-2'}>
                 {email}
