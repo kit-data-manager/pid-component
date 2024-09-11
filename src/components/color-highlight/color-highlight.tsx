901cbd6aa6087050de0f8cf45da9aa9d4132817c
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2024-09-11 14:03:15 +0200

    extracted color-generated text from HandleType

---
+++ src/components/color-highlight/color-highlight.tsx
@@ -0,0 +1,42 @@
+import {Component, h, Host, Prop, State} from '@stencil/core';
+import {HSLColor} from "./HSLColor";
+
+@Component({
+  tag: 'color-highlight',
+  styleUrl: 'color-highlight.css',
+  shadow: true,
+})
+export class ColorHighlight {
+
+  /**
+   * The text to highlight.
+   * @type {string}
+   */
+  @Prop() text: string;
+
+  /**
+   * The color of the text.
+   * @private
+   * @type {HSLColor}
+   */
+  @State() color: HSLColor;
+
+  async componentWillLoad() {
+    this.color = await HSLColor.generateColor(this.text);
+  }
+
+  render() {
+    return (
+      <Host>
+        <span
+          style={{
+            color: 'hsl(' + this.color.hue + ',' + this.color.sat + '%,' + this.color.lum + '%)',
+          }}
+          class={`font-mono font-bold rounded-md`}
+        >
+          {this.text}
+        </span>
+      </Host>
+    );
+  }
+}
---
+++ src/components/color-highlight/readme.md
@@ -0,0 +1,17 @@
+# handle-highlight
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description            | Type     | Default     |
+| -------- | --------- | ---------------------- | -------- | ----------- |
+| `text`   | `text`    | The text to highlight. | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
