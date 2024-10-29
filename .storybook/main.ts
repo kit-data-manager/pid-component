a45e616596c0394eaa3cde55719ff9f94b804f7a
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2024-10-29 09:48:14 +0100

    bugfix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,15 @@
 export default {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
+  ],
   staticDirs: [{ from: '../dist', to: '/assets' }, { from: '../loader', to: '/assets' }],
   framework: '@storybook/web-components-vite',
   docs: {
     source: {
       type: 'code',
-    },
+    }
   },
   core: {
     disableTelemetry: true,
