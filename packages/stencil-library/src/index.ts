fb3811d593347d3cabc460b8a6c1d169ca4c64c7
Christopher Raquet <christopher.raquet@kit.edu>
2026-02-10 09:52:45 +0100

    fix: also export PIDDataType and PIDRecord

--- packages/stencil-library/src/index.ts
+++ packages/stencil-library/src/index.ts
@@ -1,2 +1,4 @@
 export * from './components';
 export { PID } from './rendererModules/Handle/PID'
+export { PIDDataType } from './rendererModules/Handle/PIDDataType'
+export { PIDRecord } from './rendererModules/Handle/PIDRecord'
